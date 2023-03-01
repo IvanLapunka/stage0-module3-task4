2b4e61c0e5fe31a783254e89c6a12007e9e1b7ad
Ivan Lapunka <ivan_lapunka@gmail.com>
2023-03-01 10:08:37 +0300

    Changes

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a = 1;
+        double b = 2;
+        double m1 = (9 * a * a - 5 * b +2 + a - 7);
+        double m2 = (a + b - 4 * a * b) / 2;
+        double result = m1 * m2;
     }
 }
